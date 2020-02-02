aa19cffd577dac68c7bb4045163039e4aba8f521
gihyungkeum <58721130+gihyungkeum@users.noreply.github.com>
2020-02-02 19:20:00 +0900

    Add files via upload

---
+++ AtomEditor.py
@@ -0,0 +1,20 @@
+print('Hello World!, Hi ICHCAP!!')
+ichcap = "unesco categoyII institute for ICH information sharing and network building for Asian and the Pacific regions"
+print('ichcap')
+print(2020 02 11)
+
+'''(검색) atom editor window  : https://atom.io/  ....> download
+1. file --> setting -->, & install --> install package : python, script...
+2. https://www.inflearn.com/course/python-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%9E%85%EB%AC%B8-%EA%B0%95%EC%A2%8C/lecture/8324
+
+(atom 실행방법)
+1. 터미널(커맨드)창 열기:  (window) + R --> cmd --> atom .
+(1) 기초명령어: >cd, >cd.., >mkdir
+(2) 아톰 터미널 환경(터미널 창)에서 윈도우 환경(로컬드라이브/내 pc 환경) 열기 : (window) + e
+  (2-1) 아톰 터미널 환경과 윈도우 환경 연도 : mkdir을 통해 폴더 생성
+  (2-2) 윈도우 환경에서 우클릭 --> 새로만들기 --> 텍스트 파일생성 : 아톰에디터에도 연동되어 파일생성
+2. 아톰 에디터에서 바로 파일생성하여 실행 : >atom 파일명.py --> 엔터
+3. 터미널 창이 작동 안 될 경우-->위도우 환경에 우클릭 : open with atom
+
+(atom 단축키) : https://yunipapa.blogspot.com/2017/06/atom.html
+'''
